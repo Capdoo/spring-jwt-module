e1d6fcdb84363ae1557fafaeefaa64b5a1fed616
Capdoo <mazher_2014@hotmail.com>
2021-10-09 06:07:36 -0500

    jwt spring

---
+++ src/main/java/com/example/app/PruebajwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebajwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebajwtApplication.class, args);
+	}
+
+}
